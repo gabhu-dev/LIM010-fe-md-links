d90c1c4dff3e9f560617d96c9e67ed6ee925fb50
gabhu <gabhu@hotmail.es>
2019-09-17 16:47:54 -0500

    agregando carpeta lib, funcion isFileOrDirectory arreglada y un correcto test

--- test/.test.spec.js
+++ test/.test.spec.js
@@ -4,58 +4,51 @@ import { isAbsoluteOrRelative, existRoute, extensions, isFileOrDirectory, saveEx
 
 
 describe('Esta función debería saber distinguir la absoluta de la relativa', () => {
-    it('deberia poder distinguir una absoluta', () => {
-        isAbsoluteOrRelative('C:\Users\Files\gabhu.js');
-        expect('C:\Users\Files\gabhu.js').toBe('C:\Users\Files\gabhu.js');
-    })
-    it('deberia poder distinguir una relativa', () => {
-        isAbsoluteOrRelative('..\gabhu.js');
-        expect('C:\Users\Files\gabhu.js').toBe('C:\Users\Files\gabhu.js');
-    })
+it('deberia poder distinguir una absoluta', () => {
+  expect(isAbsoluteOrRelative(process.cwd())).toBe(process.cwd());
+})
+it('deberia poder distinguir una relativa', () => {
+  expect(isAbsoluteOrRelative('src\\index.js')).toBe('C:\\Users\\LABORATORIA D0082\\Desktop\\project markdown\\LIM010-fe-md-links\\src\\index.js');
+})
 })
 
 describe('Esta función debería poder determinar si existe la ruta', () => {
-    it('debería tener como resultado <si existe> cuando la ruta es verdadera', () => {
-        existRoute('C:\Users\gabhu\Desktop\markdown');
-        expect('si existe').toBe('si existe');
-    })
-    it('deberia tener como resultado no existe', () => {
-        existRoute('C:\Users\gabhu\Desktop\markdow');
-        expect('no existe').toBe('no existe');
-    })
+  it('debería tener como resultado <si existe> cuando la ruta es verdadera', () => {
+    expect(existRoute(process.cwd())).toBe(true);
+  })
+  it('deberia tener como resultado no existe', () => {
+    expect(existRoute('C:\\Users\\gabhu\\Desktop\\markdown')).toBe(false);
+  })
 })
 
 describe('Esta función debería poder determinar si es un archivo', () => {
-    it('existe archivo', () => {
-        isFileOrDirectory('C:\Users\gabhu\Desktop\markdown\LIM010-fe-md-links\src\index.js');
-        expect('es archivo').toBe('es archivo');
-    })
-    it('existe directorio', () => {
-        isFileOrDirectory('C:\Users\gabhu\Desktop\markdown\LIM010-fe-md-links\src');
-        expect('es directorio').toBe('es directorio');
-    })
+  it('existe archivo', () => {
+    expect(isFileOrDirectory('C:\\Users\\LABORATORIA D0082\\Desktop\\project markdown\\LIM010-fe-md-links\\src\\index.js')).toBe(true);
+  })
+  it('existe directorio', () => {
+    expect(isFileOrDirectory('C:\\Users\\LABORATORIA D0082\\Desktop\\project markdown\\LIM010-fe-md-links\\src')).toBe(false);
+  })
 })
 
 describe(' function extensions', () => {
-    it('deberia poder obtener la extension de la  ruta', () => {
-        extensions('C:\Users\gabhu\Desktop\markdown\LIM010-fe-md-links\README.md');
-        expect('.md').toBe('.md');
-    })
-    it('deberia salir error porque no se encontró extension', () => {
-        extensions('C:\Users\gabhu\Desktop\markdown\LIM010-fe-md-links\src');
-        expect('no hay extensiones').toBe('no hay extensiones');
-    })
+  it('deberia poder obtener la extension de la  ruta', () => {
+    expect(extensions('C:\\Users\\LABORATORIA D0082\\Desktop\\project markdown\\LIM010-fe-md-links\\README.md')).toBe('.md');
+  })
+  it('deberia salir error porque no se encontró extension', () => {
+    extensions('C:\Users\gabhu\Desktop\markdown\LIM010-fe-md-links\src');
+    expect('no hay extensiones').toBe('no hay extensiones');
+  })
 })
 describe('funcion saveExtensions', () => {
-    it('deberia poder guardar en el array', () => {
-        saveFiles('README.md');
-        expect('README.md').toBe('README.md');
-    })
+  it('deberia poder guardar en el array', () => {
+    saveFiles('README.md');
+    expect('README.md').toBe('README.md');
+  })
 })
 
-describe('funcion readFiles', () => {
-    it('debería poder leer un archivo', () => {
-        readFiles('README.md');
-        expect()
-    })
-})
+// describe('funcion readFiles', () => {
+//     it('debería poder leer un archivo', () => {
+//         readFiles('README.md');
+//         expect()
+//     })
+// })
