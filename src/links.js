dcd9c692f2ca63c9c57b5cb243a932b5ed7e75da
gabhu-dev <gabhu@hotmail.es>
2019-09-25 11:33:58 -0500

    agregando cli.js y ordenando test fetch

--- src/links.js
+++ src/links.js
@@ -3,33 +3,32 @@ const marked = require('marked');
 const fetch = require('node-fetch');
 
 export const extLinks = (arrayFiles) => {
-  const arrayOfLinks = [];
+    const arrayOfLinks = [];
     arrayFiles.forEach((file) => {
-      const readFile = fs.readFileSync(file, 'utf8');
-      const render = new marked.Renderer();
+        const readFile = fs.readFileSync(file, 'utf8');
+        const render = new marked.Renderer();
         render.link = (href, title, text) => {
-          arrayOfLinks.push({
-            href,
-            text,
-            file:file,
-          });
+            arrayOfLinks.push({
+                href,
+                text,
+                file,
+            });
         };
-      marked(readFile, { renderer: render });
-
+        marked(readFile, { renderer: render });
     });
     return arrayOfLinks;
 };
-// const arrayFiles = [
-//   'C:\\Users\\LABORATORIA D0082\\Desktop\\project markdown\\LIM010-fe-md-links\\lib\\readme.md','C:\\Users\\LABORATORIA D0082\\Desktop\\project markdown\\LIM010-fe-md-links\\lib\\readme-2.md'
-// ]
-// console.log(extLinks(arrayFiles));
+const arrayFiles = [
+    'C:\\Users\\gabhu\\Desktop\\markdown\\LIM010-fe-md-links\\lib\\readme.md', 'C:\\Users\\gabhu\\Desktop\\markdown\\LIM010-fe-md-links\\lib\\readme-2.md'
+]
+console.log(extLinks(arrayFiles));
 
 export const validateLink = (arrayOfLinks) => {
-    const promiseLink = arrayOfLinks.map((elem) => fetch(elem.href)
+    const promiseLink = arrayOfLinks.map(elem => fetch(elem.href)
         // response es devuelto cuando fetch ha sido satisfactoria
         .then((response) => {
             // respuesta satisfactoria
-            if (response.status >= 200 && response.status <= 226) {
+            if (response.status >= 200 && response.status < 400) {
                 return {
                     ...elem,
                     statusText: response.statusText, // ok
@@ -44,34 +43,31 @@ export const validateLink = (arrayOfLinks) => {
             };
         })
         // problemas en la peticion de fetch
-        .catch((err) => {
-            return err;
-        }));
+        .catch(err => err));
     return Promise.all(promiseLink);
 };
-const arrayOfLinks = [ 
-{ href: 'https://nodejs.org/es/',
-text: 'Node.js',
-file:'C:\\Users\\LABORATORIA D0082\\Desktop\\project markdown\\LIM010-fe-md-links\\lib\\readme.md' },
-{href: 'https://nodejs.org/es/',
-text: 'Node.js',
-file:'C:\\Users\\LABORATORIA D0082\\Desktop\\project markdown\\LIM010-fe-md-links\\lib\\readme.md' }
-]
+const arrayLinks = [{
+        href: 'https://nodejs.org/es/',
+        text: 'Node.js',
+        file: 'C:\\Users\\LABORATORIA D0082\\Desktop\\project markdown\\LIM010-fe-md-links\\lib\\readme.md',
+    },
+    {
+        href: 'https://nodejs.org/es/',
+        text: 'Node.js',
+        file: 'C:\\Users\\LABORATORIA D0082\\Desktop\\project markdown\\LIM010-fe-md-links\\lib\\readme.md',
+    },
+];
 
-// validateLink(arrayOfLinks)
-// .then((res)=>{
-//   console.log(res);
-// }) 
-// funcion stats -- verificar
-export const statsLink = (arrayOfLinks) => {
-  const total = arrayOfLinks.length;
-  const unique = arrayOfLinks.filter((item, index, array) => {
-    return array.indexOf(item) === index;
-   })
-    return { Total: total, Unique: unique.length }
-}
-console.log(statsLink(arrayOfLinks));
-// funcion stats-validate
-export const statsValidate = (statsLink,brokens) => {
-return {statsLink,}
-}
+validateLink(arrayLinks)
+    .then((res) => {
+        console.log(res);
+    })
+    // funcion stats -- verificar
+    // export const statsLink = (arrayOfLinks) => {
+    //     const total = arrayOfLinks.length;
+    //     const unique = arrayOfLinks.filter((item, index, array) => array.indexOf(item) === index);
+    //     return { Total: total, Unique: unique.length };
+    // };
+    // console.log(statsLink(arrayLinks));
+    // // funcion stats-validate
+    // export const statsValidate = (statsLink, brokens) => ({ statsLink });
--- test/.link.spec.js
+++ test/.link.spec.js
@@ -1,40 +1,34 @@
 import { extLinks, validateLink, statsLink } from '../src/links.js';
 // crear unn array
 const arrayFiles = [
-  'C:\\Users\\LABORATORIA D0082\\Desktop\\project markdown\\LIM010-fe-md-links\\lib\\readme.md',
-]
-const arrayOfLinks = [ 
-  { href: 'https://nodejs.org/es/',
-  text: 'Node.js',
-  file:'C:\\Users\\LABORATORIA D0082\\Desktop\\project markdown\\LIM010-fe-md-links\\lib\\readme.md' },
-  ]
+    // 'C:\\Users\\LABORATORIA D0082\\Desktop\\project markdown\\LIM010-fe-md-links\\lib\\readme.md',
+    'C:\\Users\\gabhu\\Desktop\\markdown\\LIM010-fe-md-links\\lib\\readme.md'
+];
+
+const arrayOfLinks = [{
+    href: 'https://nodejs.org/es/',
+    text: 'Node.js',
+    'C:\\Users\\gabhu\\Desktop\\markdown\\LIM010-fe-md-links\\lib\\readme.md'
+}, ];
+
+
 describe('funcion extLinks', () => {
-  it('deberia poder leer y extraer todos los links', () => {
-    expect(extLinks(arrayFiles)).toStrictEqual([ { href: 'https://nodejs.org/es/', text: 'Node.js',file:'C:\\Users\\LABORATORIA D0082\\Desktop\\project markdown\\LIM010-fe-md-links\\lib\\readme.md' }]);
-  })
+    it('deberia poder leer y extraer todos los links', () => {
+        expect(extLinks(arrayFiles)[0].text).toBe('Node.js');
+    })
 })
 
-// describe('funcion de status fetch', () => {
-//   it('deberia poder saber el status de un link', () => {
-//     expect(validateLink('https://developer.mozilla.org/es/docs/Web/API/Fetch_API/Utilizando_Fetch')).toBe()
-//   })
+describe('funcion validate Link', () => {
+    it('deberia retornar un array con objeto de 5 propiedades', () => {
+        validateLink(arrayOfLinks)
+            .then((res) => {
+                expect(res[0].statusText).toBe('OK');
+            })
+    });
+});
+
+// describe('funcion statsLink', () => {
+//     it('deberia retornar un array', () => {
+//         expect(statsLink(arrayOfLinks).toBe())
+//     })
 // })
-describe('funcion validate Link',()=>{
-  it('deberia retornar un array con objeto de 5 propiedades',()=>{
-    validateLink(arrayOfLinks)
-    .then((res) =>{
-      expect(res).toBe([ { href: 'https://nodejs.org/es/',
-      text: 'Node.js',
-      file:
-       'C:\\Users\\LABORATORIA D0082\\Desktop\\project markdown\\LIM010-fe-md-links\\lib\\readme.md',
-      statusText: 'OK',
-      status: 200 } ]);
-  })
-});
-});
-
-describe('funcion statsLink',()=>{
-  it('deberia retornar un array',()=>{
-    expect(statsLink(arrayOfLinks).toBe())
-  })
-})
