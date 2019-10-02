b585289f63690bef07acc292279b18d6986b71a5
gabhu <gabhu@hotmail.es>
2019-10-02 15:11:18 -0500

    arreglando slint

--- test/.link.spec.js
+++ test/.link.spec.js
@@ -3,62 +3,62 @@ import { extLinks, validateLink, statsLink, statsValidate } from '../src/links.j
 const path = require('path');
 // crear unn array
 const arrayFiles = [
-    path.join(process.cwd(), 'pruebas', 'pruebaREADME.md')
+  path.join(process.cwd(), 'pruebas', 'pruebaREADME.md')
 ];
 
 const arrayOfLinks = [{
-    href: 'https://nodejs.org/es/',
-    text: 'Node.js',
-    file: path.join(process.cwd(), 'pruebas', 'pruebaREADME.md'),
+  href: 'https://nodejs.org/es/',
+  text: 'Node.js',
+  file: path.join(process.cwd(), 'pruebas', 'pruebaREADME.md'),
 }, ];
 
 const arrayOfLinksBad = [{
-    href: 'https://nodejs.org/s/',
-    text: 'Node.js',
-    file: 'C:\\Users\\LABORATORIA D0082\\Desktop\\project markdown\\LIM010-fe-md-links\\lib\\readme.md',
+  href: 'https://nodejs.org/s/',
+  text: 'Node.js',
+  file: 'C:\\Users\\LABORATORIA D0082\\Desktop\\project markdown\\LIM010-fe-md-links\\lib\\readme.md',
 }, ]
 
 const linksBad = [{
-    href: 'https://nodejs.org/s/',
-    text: 'Node.js',
-    file: path.join(process.cwd(), 'pruebas', 'pruebaREADME.md'),
-    statusText: 'fail',
-    status: 404
+  href: 'https://nodejs.org/s/',
+  text: 'Node.js',
+  file: path.join(process.cwd(), 'pruebas', 'pruebaREADME.md'),
+  statusText: 'fail',
+  status: 404
 }]
 
 const resultStat = 'Total : 2\nUnique : 2';
 
 describe('funcion extLinks', () => {
-    it('deberia poder leer y extraer todos los links', () => {
-        expect(extLinks(arrayFiles)[0].text).toBe('Node.js');
-    })
+  it('deberia poder leer y extraer todos los links', () => {
+    expect(extLinks(arrayFiles)[0].text).toBe('Node.js');
+  })
 })
 
 describe('funcion validate Link', () => {
-    it('deberia retornar un status con 200', (done) => {
-        validateLink(arrayOfLinks)
-            .then((res) => {
-                expect(res[0].status).toBe(200);
-                done()
-            })
+  it('deberia retornar un status con 200', (done) => {
+    validateLink(arrayOfLinks)
+      .then((res) => {
+        expect(res[0].status).toBe(200);
+        done()
+      })
     });
     it('deberia retornar un statusText', (done) => {
-        validateLink(arrayOfLinksBad)
-            .then((res) => {
-                expect(res[0].statusText).toBe('fail');
-                done()
-            })
-    });
+    validateLink(arrayOfLinksBad)
+      .then((res) => {
+        expect(res[0].statusText).toBe('fail');
+        done()
+      })
+  });
 });
 
 describe('funcion statsLink', () => {
-    it('deberia retornar un objeto con 2 propiedades', () => {
-        expect(statsLink(arrayOfLinks)).toBe('Total : 1\nUnique : 1');
-    })
+  it('deberia retornar un objeto con 2 propiedades', () => {
+    expect(statsLink(arrayOfLinks)).toBe('Total : 1\nUnique : 1');
+  })
 })
 
 describe('funcion statsValidate', () => {
-    it('deberia retornar total,unique,brokens', () => {
-        expect(statsValidate(linksBad, resultStat)).toBe('Total : 2\nUnique : 2\nBroken : 1')
-    })
+  it('deberia retornar total,unique,brokens', () => {
+    expect(statsValidate(linksBad, resultStat)).toBe('Total : 2\nUnique : 2\nBroken : 1')
+  })
 })
--- test/.md-Links.spec.js
+++ test/.md-Links.spec.js
@@ -3,17 +3,17 @@ const path = require('path');
 
 describe('funcion principal', () => {
   it('cuando tiene ruta y option.validate es false', (done) => {
-    mdLinks(path.join(process.cwd(), 'pruebas', 'pruebaREADME.md'), { validate: false })
-        .then(res => {
-            expect(res[0].text).toBe('Node.js');
-            done()
-        })
+  mdLinks(path.join(process.cwd(), 'pruebas', 'pruebaREADME.md'), { validate: false })
+    .then(res => {
+        expect(res[0].text).toBe('Node.js');
+        done()
+      })
   })
   it('cuando tiene ruta y option.validate es true', (done) => {
-    mdLinks(path.join(process.cwd(), 'pruebas', 'pruebaREADME.md'), { validate: true })
-        .then(res => {
-            expect(res[0].status).toBe(200);
-            done()
-        })
-  })
+  mdLinks(path.join(process.cwd(), 'pruebas', 'pruebaREADME.md'), { validate: true })
+    .then(res => {
+      expect(res[0].status).toBe(200);
+      done()
+    })
+})
 })
